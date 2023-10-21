7cd80b9e27c93f69fe5c3742c373b146fc4356a7
Ilya Selenzev <blizardsis@yandex.ru>
2023-10-21 22:33:13 +0300

    1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -17,13 +17,22 @@ def hello_author() -> dict[str, str]:
     return {'Hello': 'author'}
 
 
-@app.get('/{name}')
+@app.get(
+    '/{name}',
+    tags=['common methods'],
+    summary='Общее приветствие',
+    response_description='Полная строка приветствия'
+)
 def greetings(
-        name: str,
-        surname: str,
-        age: Optional[int] = None,
-        is_staff: bool = False,
-        education_level: Optional[EducationLevel] = None,
+        *,
+        name: str = Path(
+            ..., min_length=2, max_length=20,
+            title='Полное имя', description='Можно вводить в любом регистре'
+        ),
+        surname: str = Query(..., min_length=2, max_length=50),
+        age: Optional[int] = Query(None, gt=4, le=99),
+        is_staff: bool = Query(False, alias='is-staff'),
+        education_level: Optional[EducationLevel] = None, 
 ) -> dict[str, str]:
     result = ' '.join([name, surname])
     result = result.title()
