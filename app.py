3f19ef88498edf035c201515261ff22d4a1f6814
Ilya Selenzev <blizardsis@yandex.ru>
2023-10-24 13:37:51 +0300

    2

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from fastapi import FastAPI, Query
+from enum import Enum
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/math-sum')
-def default(
-        add: list[float] = Query(..., gt=0, lt=10),
-) -> float:
-    result = 0
-    for ad in add:
-        result += ad
-    return result
+class Category(str, Enum):
+    PRINTER = 'Принтеры'
+    MONITOR = 'Мониторы'
+    ADDITIONAL = 'Доп. оборудование'
+    IODEV = 'Устройства ввода'
+
+
+class Person(BaseModel):
+    name: str
+    surname: str
+    age: Optional[int]
+    is_staff: bool = False
+
+
+class AuctionLot(BaseModel):
+    category: Category
+    name: str
+    model: Optional[str]
+    start_price: int = 1000
+    seller: Person
+
+
+@app.post('/new-lot')
+def register_lot(lot: AuctionLot) -> dict[str, str]:
+    return {'result': 'Ваша заявка зарегистрирована!'}
--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-from enum import Enum
-from typing import Optional, Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from schemas import Person
 
 app = FastAPI()
-
-
-class EducationLevel(str, Enum):
-    SECONDARY = 'Среднее образование'
-    SPECIAL = 'Среднее специальное образование'
-    HIGHER = 'Высшее образование'
-
-
-class Person(BaseModel):
-    name: str
-    surname: Union[str, list[str]]
-    age: Optional[int]
-    is_staff: bool = False
-    education_level: Optional[EducationLevel]
 
 
 @app.post('/hello')
@@ -28,7 +12,6 @@ def greetings(person: Person) -> dict[str, str]:
     else:
         surnames = person.surname
     result = ' '.join([person.name, surnames])
-    result = result.title()
     if person.age is not None:
         result += ', ' + str(person.age)
     if person.education_level is not None:
