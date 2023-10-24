dfb0379a5dee293c1c8cc588f0b89144dc918edd
Ilya Selenzev <blizardsis@yandex.ru>
2023-10-24 16:15:56 +0300

    refactor

--- app/main.py
+++ app/main.py
@@ -1,25 +1,7 @@
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
 
-from app.schemas.schemas import Person
+from app.api.endpoints import router
 
 app = FastAPI()
 
-
-@app.post('/hello')
-def greetings(
-        person: Person = Body(
-            ..., examples=Person.Config.schema_extra['examples']
-        )
-) -> dict[str, str]:
-    if isinstance(person.surname, list):
-        surnames = ' '.join(person.surname)
-    else:
-        surnames = person.surname
-    result = ' '.join([person.name, surnames])
-    if person.age is not None:
-        result += ', ' + str(person.age)
-    if person.education_level is not None:
-        result += ', ' + person.education_level.lower()
-    if person.is_staff:
-        result += ', сотрудник'
-    return {'Hello': result}
+app.include_router(router)
